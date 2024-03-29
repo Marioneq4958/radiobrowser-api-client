358c0f0f6e9793a992ddb689fd13502969c6c3c5
Marioneq 4958 <mq4958@gmail.com>
2024-03-29 20:57:37 +0100

    Add Client.searchStations() and Client.fetchStations()

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,8 @@
 import { Axios } from 'axios';
+import { Station } from './types/data';
+import { ISearchStation } from './types/queries';
+import { StationsListOutputFormat } from './types/formats';
+import { JsonSerializer } from 'typescript-json-serializer';
 
 export class RadioBrowserClient {
   private readonly axios: Axios;
@@ -10,7 +14,7 @@ export class RadioBrowserClient {
   }
 
   private async getServers() {
-    const data = JSON.parse((await this.axios.get('http://all.api.radio-browser.info/json/servers')).data);
+    const data = JSON.parse((await this.axios.get('http://de1.api.radio-browser.info/json/servers')).data);
     const servers = data.map((server: { name: string }) => 'https://' + server.name);
     if (servers.length === 0) {
       throw new Error('No servers');
@@ -26,5 +30,39 @@ export class RadioBrowserClient {
       params,
     });
     return response;
+  }
+
+  public async fetchStations(params: ISearchStation, outputFormat: StationsListOutputFormat = 'json'): Promise<string> {
+    const { data } = await this.sendRequest<string>('stations/search', outputFormat, {
+      name: params.name,
+      nameExtact: params.nameExtact,
+      country: params.country,
+      countryExact: params.countryExact,
+      countrycode: params.countryCode,
+      state: params.state,
+      stateExact: params.stateExact,
+      language: params.language,
+      languageExact: params.languageExact,
+      tag: params.tag,
+      tagEaxct: params.tagEaxct,
+      tagList: params.tagList?.join(','),
+      codec: params.codec,
+      bitrateMin: params.bitrateMin,
+      bitrateMax: params.bitrateMax,
+      has_geo_info: params.hasGeoInfo,
+      has_extended_info: params.hasExtenedInfo,
+      is_https: params.isHttps,
+      order: params.order,
+      reverse: params.reverse,
+      offset: params.offset,
+      limit: params.limit,
+      hidebroken: params.hideBroken,
+    });
+    return data;
+  }
+
+  public async searchStations(params: ISearchStation): Promise<Station[]> {
+    const data = await this.fetchStations(params);
+    return JSON.parse(data).map((station: any) => new JsonSerializer().deserialize(station, Station));
   }
 }
---
+++ src/types/data.ts
@@ -0,0 +1,88 @@
+import { JsonProperty, JsonObject } from 'typescript-json-serializer';
+
+@JsonObject()
+export class Station {
+  @JsonProperty({ name: 'changeuuid' })
+  public changeUUID: string;
+
+  @JsonProperty({ name: 'stationuuid' })
+  public stationUUID: string;
+
+  @JsonProperty()
+  public name: string;
+
+  @JsonProperty()
+  public url: string;
+
+  @JsonProperty({ name: 'url_resolved' })
+  public urlResolved: string;
+
+  @JsonProperty({ name: 'homepage' })
+  public homepageUrl: string;
+
+  @JsonProperty({ name: 'favicon' })
+  public faviconUrl: string | null;
+
+  @JsonProperty({ name: 'tags', beforeDeserialize: (value: string) => value.split(',') })
+  public tags: string[];
+
+  @JsonProperty({ name: 'country' })
+  public countryName: string;
+
+  @JsonProperty({ name: 'countrycode' })
+  public countryCode: string;
+
+  @JsonProperty()
+  public state: string;
+
+  @JsonProperty({ name: 'language' })
+  public languageNames: string;
+
+  @JsonProperty({ name: 'languagecodes' })
+  public languageCodes: string;
+
+  @JsonProperty()
+  public votes: number;
+
+  @JsonProperty({ name: 'lastchangetime_iso8601' })
+  public lastChangeTime: Date;
+
+  @JsonProperty()
+  public codec: string;
+
+  @JsonProperty()
+  public bitrate: number;
+
+  @JsonProperty({ beforeDeserialize: (value) => Boolean(value) })
+  public hls: boolean;
+
+  @JsonProperty({ name: 'lastcheckok', beforeDeserialize: (value: number) => Boolean(value) })
+  public lastCheckOk: boolean;
+
+  @JsonProperty({ name: 'lastchecktime_iso8601' })
+  public lastCheckDate: Date;
+
+  @JsonProperty({ name: 'lastcheckoktime_iso8601' })
+  public lastCheckOkDate: Date;
+
+  @JsonProperty({ name: 'clicktimestamp_iso8601' })
+  public lastClickDate: Date;
+
+  @JsonProperty({ name: 'clickcount' })
+  public clickCount: number;
+
+  @JsonProperty({ name: 'clicktrend' })
+  public clickTrend: number;
+
+  @JsonProperty({ name: 'ssl_error', beforeDeserialize: (value: number) => Boolean(value) })
+  public sslError: boolean;
+
+  @JsonProperty({ name: 'geo_lat' })
+  public geoLat: number | null;
+
+  @JsonProperty({ name: 'geo_long' })
+  public geoLong: number | null;
+
+  @JsonProperty({ name: 'has_extended_info' })
+  public hasExtendedInfo: boolean | null;
+}
