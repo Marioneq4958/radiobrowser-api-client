bd4153551448fa16777e53b40c2df13638bf08f6
Marioneq 4958 <mq4958@gmail.com>
2024-04-01 20:00:55 +0200

    Add client.getTags and client.getLanguages tests

--- tests/test.ts
+++ tests/test.ts
@@ -7,12 +7,16 @@ import JsonStationsSearch from './fixtures/json-stations-search.json';
 import JsonCodecs from './fixtures/json-codecs.json';
 import JsonCountries from './fixtures/json-countries.json';
 import JsonStates from './fixtures/json-states.json';
+import JsonLanguages from './fixtures/json-languages.json';
+import JsonTags from './fixtures/json-tags.json';
 
 // Expected
 import SearchStations from './expected/search-stations.json';
 import GetCodecs from './expected/get-codecs.json';
 import GetCountires from './expected/get-countries.json';
 import GetStates from './expected/get-states.json';
+import GetLanguages from './expected/get-languages.json';
+import GetTags from './expected/get-tags.json';
 
 describe('sum test', () => {
   let client: RadioBrowserClient;
@@ -27,7 +31,11 @@ describe('sum test', () => {
       .get('/json/countries/?limit=20')
       .reply(200, JsonCountries)
       .get('/json/states/?limit=20')
-      .reply(200, JsonStates);
+      .reply(200, JsonStates)
+      .get('/json/languages/?limit=20')
+      .reply(200, JsonLanguages)
+      .get('/json/tags/?limit=20')
+      .reply(200, JsonTags);
     client = new RadioBrowserClient('MyRadioApp', '0.1.0');
   });
   it('search stations', async () => {
@@ -45,5 +53,13 @@ describe('sum test', () => {
   it('get states', async () => {
     const data = await client.getStates({ limit: 20 });
     expect(JSON.parse(JSON.stringify(data))).toStrictEqual(GetStates);
+  });
+  it('get languages', async () => {
+    const data = await client.getLanguages({ limit: 20 });
+    expect(JSON.parse(JSON.stringify(data))).toStrictEqual(GetLanguages);
+  });
+  it('get tags', async () => {
+    const data = await client.getTags({ limit: 20 });
+    expect(JSON.parse(JSON.stringify(data))).toStrictEqual(GetTags);
   });
 });
